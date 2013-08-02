a8a9470e1bf84c06a86c9d44361c725b7c70c1d6
Seymour Makarov <seymour.makarov@gmail.com>
2013-08-02 16:15:28 +0400

    filtering

--- newsfilter.py
+++ newsfilter.py
@@ -14,9 +14,18 @@ class NewsProcessor:
             res = urllib2.urlopen(req)
             self.status = res.code
             if(self.status == 200):
-                self.readConfig()
-                #self.data = res.read()
-
+                self.config = Cfg("process.cfg")
+                self.data = res.read()
+                res = re.search(r'(?is)(?:<body[^>]*>)(?P<data>.*)(?:</body>)',self.data)
+                if (res):
+                    self.data = res.groupdict()["data"]
+                inclPttrn = r"(?is)(?:<[^>]+\s+(id|class)=\S*("+self.config["filter"]["include"].replace(",","|")+")[^>]*>)"
+                res = re.search(inclPttrn,self.data)
+                #print self.data
+                if(res):
+                    self.data=self.data[res.start():]
+                    print self.data[:50]
+                exit
             elif (self.status >= 400 and self.status < 500):
                 print "Got a client error while accessing ", self.link
             elif (self.status >= 500):
@@ -30,7 +39,7 @@ class NewsProcessor:
         pass
     def readConfig(self):
         """Read the config file, fix the settings"""
-        pass
+
     def formPath(self):
         """Form the path to save the file with the filtered and formatted data in accordance with the link provided"""
         pass
@@ -41,7 +50,40 @@ class NewsProcessor:
         """Save the filtered and formatted data"""
         pass
 
+class Cfg(dict):
+    def __init__(self,pathToFile):
+        try:
+            fINI = open(pathToFile)
+        except IOError:
+            print pathToFile+" не открывается, нет такого или прав нехватает"
+            sys.exit()
+        lconf = fINI.readlines()
+        chars = (";", "#", "\n")
+        lconf = filter((lambda x: not x[0] in chars), lconf)
+        lconf = map((lambda x: x.strip()) ,lconf)
+        dconf = {}
+        for each in lconf:
+            if each[0] == "[":
+                base = each[1:][:-1]
+                self[base] = {}
+            else:
+                each = each.split("#")[0].strip()
+                each = each.split(";")[0].strip()
+                key, value = each.split("=", 1)
+                if("|" in value):
+                    self[base][key.strip()] = {}
+                    pars = value.strip().split("|")
+                    i = 0
+                    for item in pars:
+                        if(i+1 < len(pars) and i % 2 == 0):
+                            self[base][key.strip()][pars[i]]=pars[i+1]
+                        i=i+1
+                else:
+                    self[base][key.strip()] = value.strip()
+
+
 if(len(sys.argv) > 1):
     np = NewsProcessor(sys.argv[1])
+
 else:
     print "Please provide a URL"
